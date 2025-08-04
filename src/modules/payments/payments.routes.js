9c4260b444b90bddf04742897efb11bd906b7954
Jean Gutiérrez <141754865+Synfiniti@users.noreply.github.com>
2025-08-03 20:48:23 -0400

    Update payments.routes.js

--- src/modules/payments/payments.routes.js
+++ src/modules/payments/payments.routes.js
@@ -7,5 +7,7 @@ router.post('/create-payment-intent', paymentsController.createPaymentIntent);
 router.get('/movements/:userId', paymentsController.getMovements);
 router.post('/pagar-pasaje', paymentsController.transferToDriver);
 router.post('/retirar-saldo', paymentsController.simulateWithdraw);
+router.post('/simular-deposito', paymentsController.simulateDeposit);
+router.post('/recargar-saldo', paymentsController.simulateTopUp);
 
 export default router;
